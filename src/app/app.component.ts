01f92d499591982c224a38e2f90e23f0d1870cfd
vit <vit.blokhin@gmail.com>
2018-07-05 20:23:23 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Pipes demo app';
+
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  object = {
+    foo: "bar",
+    baz: "qux",
+    nested: {
+      x: 3,
+      numbers: [3,5,1]
+    }
+  }
 }
